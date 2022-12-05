c20261a96dcaf8929d95bf7e590d927200cc7f82
DennsHartstock <dennis.hartstock@web.de>
2022-12-05 18:21:27 +0100

    implement data loading

---
+++ app/src/main/java/com/example/kinopoiskmovies/ApiService.java
@@ -0,0 +1,16 @@
+package com.example.kinopoiskmovies;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    @GET("movie" +
+            "?token=ETJBF54-9DAMMTH-QFV40W3-8ZEZHPW" +
+            "&field=rating.imdb" +
+            "&search=7-10" +
+            "&sortField=votes.kp" +
+            "&sortType=-1" +
+            "&limit=50")
+    Single<MovieResponse> loadMovies();
+}
--- app/src/main/java/com/example/kinopoiskmovies/MainActivity.java
+++ app/src/main/java/com/example/kinopoiskmovies/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.kinopoiskmovies;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ApiFactory.apiService.loadMovies()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(movieResponse -> Log.d("MainActivity", movieResponse.toString()),
+                        Throwable::toString);
     }
-
-//    url:https://api.kinopoisk.dev/movie?token=ETJBF54-9DAMMTH-QFV40W3-8ZEZHPW
 }
